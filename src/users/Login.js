85db3b3e5b0ed5bf45936cb2d82aab096b27d256
Mohit Gaur <68635347+MohitGaur223@users.noreply.github.com>
2023-05-14 16:26:51 +0530

    login page message

--- src/users/Login.js
+++ src/users/Login.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from '../layout/Header';
+import "../style/login.css"
 
 
 const Login = () => {
@@ -47,11 +48,11 @@ const Login = () => {
       }
     }
     if (!found) {
-      alert('Incorrect email or password');
+      document.getElementById("msg").hidden = false;
     }
   };
-  
-  
+
+
 
   return (
     <div><Header />
@@ -86,6 +87,13 @@ const Login = () => {
                   value={password}
                   onChange={(e) => onInputChange(e)}
                 />
+
+                
+                <div className="reg">Don't have account &nbsp;
+                  <Link className="reg1" to="/Register">
+                    Register Here!
+                  </Link>
+                </div>
               </div>
 
               <button type="submit" className="btn btn-outline-primary">
@@ -94,6 +102,10 @@ const Login = () => {
               <Link className="btn btn-outline-danger mx-2" to="/">
                 Cancel
               </Link>
+
+              <div className='msg' id="msg" hidden>
+                  Incorrect Email or Password
+                </div>
             </form>
           </div>
         </div>
