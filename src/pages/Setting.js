89537b61b4d126b3d7507f69d3993c585eac6243
Ramkumar-2269 <ramkumarakula84@gmail.com>
2023-06-06 10:46:04 +0530

    Goal Setting component completed

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from '../layout/Navbar'
+import '../style/Setting.css'
+
 const Setting = () => {
+  const [goal, setGoal] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleGoalChange = (e) => {
+    setGoal(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("New Goal Created.")
+    // API call
+    setGoal('');
+    setDate('');
+  };
+
   return (
+    <>
     <div>
       <header>
         <Navbar />
       </header>
-      Setting
     </div>
-    
-  )
-}
+    <div className="fitness-goal-container">
+      <h2>Add Fitness Goal</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="goal">Goal:</label>
+          <input
+            type="text"
+            id="goal"
+            value={goal}
+            onChange={handleGoalChange}
+            className="form-control"
+            placeholder='Enter your goal..'
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">Date:</label>
+          <input
+            type="date"
+            id="date"
+            value={date}
+            onChange={handleDateChange}
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Add Goal</button>
+      </form>
+    </div>
+    </>
+  );
+};
 
-export default Setting
+export default Setting;
---
+++ src/pages/ViewGoals.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ViewGoals() {
+  return (
+    <div>ViewGoals</div>
+  )
+}
+
+export default ViewGoals
