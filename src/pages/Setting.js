3f695f9a23b7316f8314ed270c75f9196c3badfd
Ramkumar-2269 <ramkumarakula84@gmail.com>
2023-06-10 11:01:26 +0530

    initial commit

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,26 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import Navbar from '../layout/Navbar'
 import '../style/Setting.css'
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Setting = () => {
-  const [goal, setGoal] = useState('');
+  const [goalName, setGoal] = useState('');
+  const [description, setDescription] = useState('');
+  const [progress, setProgress] = useState(0);
   const [date, setDate] = useState('');
+
+  const {id} = useParams();
+
+  const navigate = useNavigate();
+  
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  // if(id){
+  //   document.getElementById("progress").hidden = false;
+  // }
 
   const handleGoalChange = (e) => {
     setGoal(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
   };
 
   const handleDateChange = (e) => {
     setDate(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("New Goal Created.")
-    // API call
-    setGoal('');
-    setDate('');
+  const handleProgressChange = (e) => {
+    setProgress(e.target.value);
   };
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    const user_id = user.id;
+    const goalItem = {goalName,description,progress,user_id,date};
+    console.log(goalItem);
+    if(id){
+      alert("Goal updated Successfully.");
+      axios.put("http://localhost:8080/goal"+"/"+id,goalItem)
+      .then(navigate("/view-goals"));
+    }
+    else{
+      alert("New Goal Created.");
+      axios.post("http://localhost:8080/goal",goalItem)
+      .then(navigate("/view-goals"));
+      setGoal('');
+      setDate('');
+    }
+  };
+
+  useEffect(()=>{
+    axios.get("http://localhost:8080/goal"+"/"+id)
+    .then((response) => {
+      setGoal(response.data.goalName)
+      setDescription(response.data.description)
+      setDate(response.data.date)
+      setProgress(response.data.progress)
+    }).catch(error => {
+            console.log(error)
+        })
+  },[id])
+
+
 
   return (
     <>
@@ -33,23 +80,46 @@ const Setting = () => {
       <h2>Add Fitness Goal</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="goal">Goal:</label>
+          <label>Goal:</label>
           <input
             type="text"
             id="goal"
-            value={goal}
+            value={goalName}
             onChange={handleGoalChange}
             className="form-control"
             placeholder='Enter your goal..'
           />
         </div>
         <div className="form-group">
-          <label htmlFor="date">Date:</label>
+          <label>Description:</label>
+          <input
+            type="text-area"
+            id="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            className="form-control"
+            placeholder='Description for your goal..'
+          />
+        </div>
+        <div className="form-group">
+          <label>Date:</label>
           <input
             type="date"
             id="date"
             value={date}
             onChange={handleDateChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group" id='progress_div' >
+          <label>Progress:</label>
+          <input
+            type="number"
+            id="progress"
+            min="0"
+            max="100"
+            value={progress}
+            onChange={handleProgressChange}
             className="form-control"
           />
         </div>
